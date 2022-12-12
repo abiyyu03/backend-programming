c9c5071e96f71e19c1a5298fb2b9cf10b7e1ce22
abiyyu03 <abiyyu652@gmail.com>
2022-12-12 16:27:07 +0700

    add : menambahkan pertemuan11 - intro expressJS

---
+++ pertemuan11/controllers/StudentController.js
@@ -0,0 +1,71 @@
+// TODO 3: Import data students dari folder data/students.js
+// code here
+const student = require('../data/students');
+// Membuat Class StudentController
+class StudentController {
+  index(req, res) {
+    // TODO 4: Tampilkan data students
+    // code here
+    const data = {
+      message: "Menampilkkan semua students",
+      data: student,
+    };
+
+    res.json(data);
+  }
+
+  store(req, res) {
+    const { nama } = req.body;
+
+    // TODO 5: Tambahkan data students
+    // code here
+    const newStudent = nama;
+    
+    student.push(newStudent);
+    
+    const data = {
+      message: `Menambahkan data student: ${nama}`,
+      data: student,
+    };
+
+    res.json(data);
+  }
+
+  update(req, res) {
+    const { id } = req.params;
+    const { nama } = req.body;
+
+    // TODO 6: Update data students
+    // code here
+    student[id] = nama;
+    
+    const data = {
+      message: `Mengedit student id ${id}, nama ${nama}`,
+      data: student,
+    };
+
+    res.json(data);
+  }
+
+  destroy(req, res) {
+    const { id } = req.params;
+
+    // TODO 7: Hapus data students
+    // code here
+
+    student.shift(id);
+
+    const data = {
+      message: `Menghapus student id ${id}`,
+      data: student,
+    };
+
+    res.json(data);
+  }
+}
+
+// Membuat object StudentController
+const object = new StudentController();
+
+// Export object StudentController
+module.exports = object;
